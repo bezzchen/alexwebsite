1ef914743967142c4c1b0dc72b8ec64a15b052ca
bezzchen <101853657+bezzchen@users.noreply.github.com>
2023-09-17 00:50:39 +1200

    Add files via upload

--- app.js
+++ app.js
@@ -20,6 +20,54 @@ app.get('/newmarketlocation', function (req, res) {
     console.log("LOCATION");
     res.sendFile(path.join(__dirname, '.', './client/location.html'));
 });
+app.get('/hotelfacilities', function (req, res) {
+    console.log("FACILITIES");
+    res.sendFile(path.join(__dirname, '.', './client/hotelfacilities.html'));
+});
+app.get('/events', function (req, res) {
+    console.log("EVENTS");
+    res.sendFile(path.join(__dirname, '.', './client/events.html'));
+});
+app.get('/reviews', function (req, res) {
+    console.log("REVIEWS");
+    res.sendFile(path.join(__dirname, '.', './client/reviews.html'));
+});
 app.listen(3000, function() {
     console.log('listening on port 3000');
+});
+// Import the necessary packages
+const nodemailer = require('nodemailer');
+
+// Create a transporter object
+const transporter = nodemailer.createTransport({
+  service: 'Gmail', // e.g., 'Gmail'
+  auth: {
+    user: 'barry.phasel235@gmail.com',
+    pass: 'kittenisminecraft0002',
+  },
+});
+
+// ...
+
+app.post('/contact-form-process', (req, res) => {
+  const name = req.body.Name;
+  const email = req.body.Email;
+  const message = req.body.Message;
+
+  const mailOptions = {
+    from: 'barry.phasel235@gmail.com',
+    to: 'barry.phasel235@gmail.com', // Replace with the recipient's email address
+    subject: 'New form submission',
+    text: `Name: ${name}\nEmail: ${email}\nMessage: ${message}`,
+  };
+
+  transporter.sendMail(mailOptions, (error, info) => {
+    if (error) {
+      console.error(error);
+      res.status(500).send('Error sending email');
+    } else {
+      console.log('Email sent: ' + info.response);
+      res.send('Form submission successful');
+    }
+  });
 });
