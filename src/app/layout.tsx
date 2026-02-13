c0c208634a9e1c5137c340b01f9cd299120c4316
Barry Chen <barry.phasel235@gmail.com>
2026-02-13 11:39:11 -0500

    imported footer

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,6 @@
 import "./globals.css";
 import Header from "../components/Header";
+import Footer from "../components/Footer";
 import { Inter } from "next/font/google";
 
 const inter = Inter({
@@ -17,6 +18,7 @@ export default function RootLayout({
       <body className="min-h-screen bg-white text-black">
         <Header />
         <main>{children}</main>
+        <Footer />
       </body>
     </html>
   );
