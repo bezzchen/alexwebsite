200bb7063d10f19dee0cfd1cd1f4ee605a99b8ac
Barry Chen <barry.phasel235@gmail.com>
2026-02-13 11:37:43 -0500

    created footer

---
+++ src/components/Footer.tsx
@@ -0,0 +1,40 @@
+import Image from "next/image";
+import Link from "next/link";
+
+export default function Footer () {
+    return (
+        <footer className="w-full border-b border-gray-20 bg white">
+            <div className="max-w-7xl mx-auto px-6 py-10">
+                <div className="grid grid-cols-1 md:grid-cols-3 gap-8 text-center md:text-left">
+                    <div>
+                        {/* Hotel Alert */}
+                        <h3 className="text-lg font-semibold mb-4">Hotel Alert</h3>
+                        <p className="text-sm text-gray-400">
+                        2% percent credit card fee.
+
+                        Hotel parking is subject to availability.
+
+                        “Tampering with the smoke detectors or fire alarm system may cause a fire alarm activation and will result in a call out service charge of $250.00 +GST and a fire department service charge of $1000.00 + GST. Please contact our front office before taking action by yourself if anything you are unsure of.”
+                        </p>
+                    </div>
+
+                    <div>
+                        {/* Contact */}
+                        <h3 className="text-lg font-semibold mb-4">Contact details</h3>
+                        <p className="text-sm text-gray-400">
+                        Content goes here.
+                        </p>
+                    </div>
+
+                    <div>
+                        {/* Hours */}
+                        <h3 className="text-lg font-semibold mb-4">Office Hours</h3>
+                        <p className="text-sm text-gray-400">
+                        Content goes here.
+                        </p>
+                    </div>
+                </div>
+            </div>
+        </footer>
+    )
+}
