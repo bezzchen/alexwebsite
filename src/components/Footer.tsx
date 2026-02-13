161d212e9f4e29345a7b1ad695703efdeaf71a81
Barry Chen <barry.phasel235@gmail.com>
2026-02-13 15:43:45 -0500

    added footer content

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -10,9 +10,9 @@ export default function Footer () {
                         {/* Hotel Alert */}
                         <h3 className="text-lg font-semibold mb-4">Hotel Alert</h3>
                         <p className="text-sm text-gray-400">
-                        2% percent credit card fee.
+                        2% percent credit card fee.<br /><br />
 
-                        Hotel parking is subject to availability.
+                        Hotel parking is subject to availability.<br /><br />
 
                         “Tampering with the smoke detectors or fire alarm system may cause a fire alarm activation and will result in a call out service charge of $250.00 +GST and a fire department service charge of $1000.00 + GST. Please contact our front office before taking action by yourself if anything you are unsure of.”
                         </p>
@@ -22,7 +22,14 @@ export default function Footer () {
                         {/* Contact */}
                         <h3 className="text-lg font-semibold mb-4">Contact details</h3>
                         <p className="text-sm text-gray-400">
-                        Content goes here.
+                        Alexander Inn <br />
+                        21 Alpers Avenue, Newmarket 1023, Auckland, New Zealand<br /><br />
+
+                        Freephone (NZ only)<br />
+                        0508 116 688<br /><br />
+
+                        Internationally<br />
+                        0064 9 281 2788
                         </p>
                     </div>
 
@@ -30,7 +37,10 @@ export default function Footer () {
                         {/* Hours */}
                         <h3 className="text-lg font-semibold mb-4">Office Hours</h3>
                         <p className="text-sm text-gray-400">
-                        Content goes here.
+                        Office hours<br />
+                        Monday - Friday 7.30 am - 8.00 pm <br />
+                        Saturday - Sunday & Public Holidays 9.00 am - 6.00 pm <br />
+                        After hours: Please contact the hotel directly
                         </p>
                     </div>
                 </div>
