e69bf615de48b68c46830a7d0e34c8051954cdc7
Barry Chen <barry.phasel235@gmail.com>
2026-02-17 17:39:00 -0500

    formatted text

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,7 +2,7 @@ import Image from "next/image";
 import "./globals.css";
 export default function Page() {
     return (
-      <main>
+      <main className="text-center">
         <Image 
           src="/images/0.jpg"
           alt="Hero photo"
@@ -10,15 +10,21 @@ export default function Page() {
           height={900}
           className="w-full h-auto"
         />
-        <p>
-        Alexander Inn
-        IN THE HEART OF NEWMARKET
-        Our upmarket rooms and suites located at Newmarket offer high quality modern décor and luxury beds.
+        <div className="flex flex-col gap-y-8">
+          <h1 className="pt-8 text-6xl">
+            ALEXANDER INN
+          </h1>
+          <h2 className="text-3xl text-yellow-500">
+            IN THE HEART OF NEWMARKET
+          </h2>
+          <p className="max-w-2xl mx-auto text-lg">
+          Our upmarket rooms and suites located at Newmarket offer high quality modern décor and luxury beds.
 
-        We have quiet rooms with double glazing, marble tiled bathrooms, balconies and sun seats to relax on… a quiet night's sleep… and yet… only a four-minute flat walk from your accommodation to the bustling fashion and shopping capital of Newmarket, Auckland and the many chic cafes and restaurants, just a stroll away.
+          We have quiet rooms with double glazing, marble tiled bathrooms, balconies and sun seats to relax on… a quiet night's sleep… and yet… only a four-minute flat walk from your accommodation to the bustling fashion and shopping capital of Newmarket, Auckland and the many chic cafes and restaurants, just a stroll away.
 
-        Remuera, Epsom, Sky City Casino, Vector Arena, The Museum and Domain, Alexandra Park Raceway, Public Hospitals and Parnell Art Galleries are all a five-minute drive. We are en-route and only 13km from the Auckland International Airport.
-        </p>
+          Remuera, Epsom, Sky City Casino, Vector Arena, The Museum and Domain, Alexandra Park Raceway, Public Hospitals and Parnell Art Galleries are all a five-minute drive. We are en-route and only 13km from the Auckland International Airport.
+          </p>
+        </div>
       </main>
     );
   }
