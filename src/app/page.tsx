9d07bdace90981da85bf3fb77691db7c6d0aefb8
Barry Chen <barry.phasel235@gmail.com>
2026-02-17 17:02:47 -0500

    added main paragraph

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -10,6 +10,15 @@ export default function Page() {
           height={900}
           className="w-full h-auto"
         />
+        <p>
+        Alexander Inn
+        IN THE HEART OF NEWMARKET
+        Our upmarket rooms and suites located at Newmarket offer high quality modern décor and luxury beds.
+
+        We have quiet rooms with double glazing, marble tiled bathrooms, balconies and sun seats to relax on… a quiet night's sleep… and yet… only a four-minute flat walk from your accommodation to the bustling fashion and shopping capital of Newmarket, Auckland and the many chic cafes and restaurants, just a stroll away.
+
+        Remuera, Epsom, Sky City Casino, Vector Arena, The Museum and Domain, Alexandra Park Raceway, Public Hospitals and Parnell Art Galleries are all a five-minute drive. We are en-route and only 13km from the Auckland International Airport.
+        </p>
       </main>
     );
   }
