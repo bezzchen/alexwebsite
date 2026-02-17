d0ad4de0c0f5d57339da436956da507b158633ef
Barry Chen <barry.phasel235@gmail.com>
2026-02-16 20:55:23 -0500

    added new images

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,14 @@
-import "./globals.css"
+import Image from "next/image";
+import "./globals.css";
 export default function Page() {
     return (
       <main className="pt-16">
-        Page content
+        <Image 
+          src="/image/0.jpg"
+          alt="Hero photo"
+          width={100}
+          height={40}
+        />
       </main>
     );
   }
