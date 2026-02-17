27377919db889a921af6c37a299cad13fea92adf
Barry Chen <barry.phasel235@gmail.com>
2026-02-16 21:00:53 -0500

    added hero photo

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,12 +2,13 @@ import Image from "next/image";
 import "./globals.css";
 export default function Page() {
     return (
-      <main className="pt-16">
+      <main>
         <Image 
-          src="/image/0.jpg"
+          src="/images/0.jpg"
           alt="Hero photo"
-          width={100}
-          height={40}
+          width={1600}
+          height={900}
+          className="w-full h-auto"
         />
       </main>
     );
