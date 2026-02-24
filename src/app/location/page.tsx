f5810f9eabdf885ac7e33ba05a7db48b07fd8971
Barry Chen <barry.phasel235@gmail.com>
2026-02-23 23:59:35 -0500

    added location title

--- src/app/location/page.tsx
+++ src/app/location/page.tsx
@@ -1,8 +1,8 @@
 import { GoogleMapsEmbed } from "@next/third-parties/google" 
 export default function Location() {
     return (
-      <main className="pt-16">
-        <div className="w-full h-[450px] mx-auto overflow-hidden shadow-lg pt-0">
+      <main className="pt-4">
+        <div className="w-full h-[450px] mx-auto overflow-hidden shadow-lg">
           <GoogleMapsEmbed 
             apiKey={process.env.NEXT_PUBLIC_GOOGLE_MAPS_API_KEY}
             height={450}
@@ -11,6 +11,12 @@ export default function Location() {
             q="21+Alpers+AvenueEpsom,+Auckland+1023,+New+Zealand"
           />
         </div>
+        <h1 className="pt-8 text-6xl">
+            ALEXANDER INN
+          </h1>
+          <h2 className="text-3xl text-yellow-500">
+            IN THE HEART OF NEWMARKET
+          </h2>
       </main>
     );
   }
