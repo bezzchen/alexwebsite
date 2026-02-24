91aee9cf052572fb0ef5627a03d1d52757091aa1
Barry Chen <barry.phasel235@gmail.com>
2026-02-23 23:55:37 -0500

    added location hero

--- src/app/location/page.tsx
+++ src/app/location/page.tsx
@@ -1,7 +1,16 @@
+import { GoogleMapsEmbed } from "@next/third-parties/google" 
 export default function Location() {
     return (
       <main className="pt-16">
-        Location content
+        <div className="max-w-6xl h-[450px] mx-auto rounded-4xl overflow-hidden shadow-lg">
+          <GoogleMapsEmbed 
+            apiKey={process.env.NEXT_PUBLIC_GOOGLE_MAPS_API_KEY}
+            height={450}
+            width="100%"
+            mode="place"
+            q="21+Alpers+AvenueEpsom,+Auckland+1023,+New+Zealand"
+          />
+        </div>
       </main>
     );
   }
