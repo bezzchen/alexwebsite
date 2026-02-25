e9a869baa8e92b63c3725cb44d31e80a64445376
Barry Chen <barry.phasel235@gmail.com>
2026-02-25 14:52:23 -0500

    added facilities content

--- src/app/facilities/page.tsx
+++ src/app/facilities/page.tsx
@@ -1,7 +1,85 @@
 export default function Facilities() {
     return (
       <main className="pt-16">
-        Facilities content
+        <div className="flex flex-col gap-y-8">
+          <h1 className="text-6xl">
+            Hotel Facilities
+          </h1>
+          <h2 className="text-3xl">
+            What we offer
+          </h2>
+          <p>
+          We have free onsite parking for all our guests - subject to availability - no reservations <br /><br />
+
+          Convenient central Auckland location <br /><br />
+
+          All rooms have Free Wi-Fi Broadband 24/7 <br /><br />
+
+          Self-contained stylishly decorated rooms & suites <br /><br />
+ 
+          Spa Suites available <br /><br />
+
+          All our rooms are serviced daily <br /><br />
+
+          10-minute drive to the central city <br /><br />
+
+          10-minute walk to City Link buses <br /><br />
+
+          10-minute walk to the Newmarket train station <br /><br />
+
+          Enroute to the airport (30 min drive) <br /><br />
+
+          5-minute walk to Auckland’s fashion capital, restaurants, cafes & cinemas <br /><br />
+
+          Lobby/Reception <br /><br />
+
+          Reception hours Monday - Friday 7.30 am - 8.00 pm <br /><br />
+
+          Reception hours Saturday, Sunday and Public Holiday 8.00 am - 6.00 pm <br /><br />
+
+          Mandarin and English-speaking staff <br /><br />
+
+          Photocopy, Scanner and postal/courier service available, charges will apply <br /><br />
+
+          Safety deposit box at reception <br /><br />
+
+          Safe security key card entry to the lobby and rooms <br /><br />
+
+          Lifts/Elevator <br /><br />
+
+          Wheelchair accessible (through car park) <br /><br />
+
+          Stairway <br /><br />
+
+          On request <br /><br />
+
+          Toothbrush set
+          
+          Soap
+
+          Shavers
+
+          Port-a-cot
+
+          Highchair
+
+          Baby Bath
+
+          Laundry service
+
+          Hair dryer
+
+          
+
+          Check In/ Check Out
+
+          Check In 2pm - 8pm Monday - Friday
+
+          Check In 2pm - 6 pm Saturday - Sunday and Public Holidays
+
+          Check Out is 10am
+          </p>
+        </div>
       </main>
     );
   }
