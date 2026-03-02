85ecd4058795efef496ab4b3daa3177928d7c1af
Barry Chen <barry.phasel235@gmail.com>
2026-03-02 16:22:38 -0500

    updated location page

--- src/app/location/page.tsx
+++ src/app/location/page.tsx
@@ -2,10 +2,10 @@ import { GoogleMapsEmbed } from "@next/third-parties/google"
 export default function Location() {
     return (
       <main className="pt-4">
-        <div className="w-full h-[450px] mx-auto overflow-hidden shadow-lg">
+        <div className="w-full h-[600px] mx-auto overflow-hidden shadow-lg">
           <GoogleMapsEmbed 
             apiKey={process.env.NEXT_PUBLIC_GOOGLE_MAPS_API_KEY}
-            height={450}
+            height={600}
             width="100%"
             mode="place"
             q="21+Alpers+AvenueEpsom,+Auckland+1023,+New+Zealand"
