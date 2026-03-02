4953a5af313df113f3809bbf08ac4c8959636bb7
Barry Chen <barry.phasel235@gmail.com>
2026-03-02 16:25:45 -0500

    added review content

--- src/app/reviews/page.tsx
+++ src/app/reviews/page.tsx
@@ -1,7 +1,111 @@
 export default function Reviews() {
     return (
       <main className="pt-16">
-        Reviews content
+        <h1 className="text-6xl">
+          Reviews
+        </h1>
+        <h2 className="text-3xl">
+          What our guests say
+        </h2>
+        <p>
+        October 23 2023 - Excellent clean hotel, your house keeping are very good.  Great location and made to welcome. Good luck with your plants you were planting.
+
+ 
+
+October 22 2023 - Overall, everything was great.  Good shower pressure nice and quiet, street wasn't loud unless the window open (Room 18), was a lovely stay.  Good for the price paid.
+
+ 
+
+May 7th 2022 - Easy parking and close to mall.
+
+Easy check in. Room was spacious. Lift available at the property. Close to mall. Free parking.
+
+ 
+
+July 1st 2022 - Excellent place to stay, clean, affordable, close to all specialist medical centres, great staff.
+
+Front office lady was super friendly and helpful. Parking was easy and accessible. Room was large, clean and comfortable. Soft bed and crisp bed sheets!
+
+Sept 2021 Room 9 - To management of Quality Suites - COVID 19 message. Hi/Bula Vinaka, Namaste - Quality Suites Management. My name is Reena Narayan. I have been deployed from Waikato Public Health Unit to help out with COVID 19 response. I just want to say Thank you for your help and support during Level 4 lockdown from your customer services team. You are Awesome! I want to say out loud that, I really enjoyed my stay in your room 9. Your accommodation facility reminded me of my fathers properties back in Fiji, same setting and very tidy. In addition, we always like to acknowledge and appreciate nice or healthy living space by just looking after it. To conclude please continue doing what you doing under COVID 19 restrictions. Good work! Well done for taking all the measures to eliminate the spread of COVID 19. Love your hard work and an Excellent Customer Services during Lockdown. Rated 10/10. Once again many thanks for having me and am looking forward to booking accommodation with you in the near future for my families. Kia kaha Auckland see you again later!
+
+August 2019 Room 17 - Had a fantastic sleep and loved it here. I was quiet and good value and luxiouriously comfy. Thank you Katz
+
+August 2019 Room 26 - Very happy with our stay here last night. Thank you for making it a very easy check in considering reception was closed by the time we got there. It was a lovely clean warm room and comfortable bed. Really enjoyed our stay and will definitely be coming here again and recommending it to friends. Katrina T
+
+20th September 2018 - My employer booked for my accommodation for my work trip. I didn't expect the room to be as cosy as this. The room was very homey. I would consider booking again for my personal trips to Auckland. Five out of 5 - Room 3
+
+October 2017 - Amazing comfortable room, really good facilities. All the staff were really nice and friendly. I would definitely stay here again and would recommend them to others. We had a great stay - Room 25
+
+October 2017 - Separate receptacles for recycling in kitchen/bathroom - Room 16
+
+28th September 2017 - Overall, great experience would definitely come back!Feels incredibly spacious for the size and love the seating options, power plugs and facilities. Minor points: no viewer indoor, no way of locking from inside of the room, poor lighting in bathroom as steamed up and took forever to all clear, poor range sky channels.
+
+August 2017 - We were very well looked after the rooms are lovely and clean and smell nice, the room service is excellent. The suites are quiet and beds are comfortable. We were extremely well looked after by all staff. Suggestions: The utensil drawer could do with a tin opener and some sharper knives for cutting and meal prep. Also the kettle leaks when you pour. Room 12
+
+27th August 2016 - Lovely room, clean, relaxing, everything you need. Great location and great customer service. Room 28
+
+19th May 2016 - Spacious, clean, modern room with attention to detail. Bathroom modern, tiled, spacious with free standing, good pressure shower. Kitchen has everything you need plus oven. Overall this motel is one of the best in Auckland
+
+March 2016 - Just a one night stopover in Auckland but would definitely stay here for any future stays. Top spot, very courteous staff - Room 28
+
+14th March 2016 - As a wheelchair user I have been thoroughly impressed with the access in the motel and our room. The bathroom is lovely and spacious with plenty of room to manage in. The staff are welcoming and friendly, especially the room attendants. Thank you for a wonderful stay. Pancake breakfast was excellent! Erin Inwood
+
+12th March 2016 - Very nice clean tidy room. Wheelchair access was excellent. Breakfast was excellent. The taps were a little weak on flow, making ablutions difficult! Room 10
+
+8th October 2015 - Beautiful spacious super clean room. Delicious food! Definitely love to come back again! Luxurious relaxing stay. Room 12
+
+13 August 2015 - Quality Suites Alexander Inn gave me a beautiful home away from home while on business. The modern rooms with great attention to detail were a lovely place to stay and a nice space to relax in after full days of meetings. Would definitely return to Quality Suites Alexander Inn - an idea for improvement would be bath robes, travelling for only a few days is hard to fit in a robe in a carry on bag, would make the experience more luxorious. Room 17
+
+24 July 2015 - A very clean, comfortable and friendly stay. I would come again. Oh and affordable for value as well. Warm & cosy.
+
+PS. The plunger coffee was great and the padding on the wonderful bed and excellent touch. Most comfortable bed ever at an overnight stay. No traffic noise due to double glazing. Room 22
+
+June 2015 - We really enjoyed our stay. Our room was well catered for and we felt our son was more than welcomed. We will stay again. Room 20
+
+8th June 2015 - Great experience. Love that they accommodated my young son without any hassles. From Andrew Surgenor
+
+7th May 2015 - Decided to stay here 3 weeks ago when coming to Auckland to visit family and friends, and chose it because of the excellent reviews. Usually we book a hotel in the CBD however we had no particular reason to need to stay right there, and I was sick of seeing the exorbitant parking costs of the 5 star hotels. Quality Suites Alexander Inn fitted the bill perfectly for us with free undercover parking. We asked for a quiet room, and that is just we got, right at the back of the complex. The room was more of an apartment with a kitchenette with a full scale oven that I'm sure had never been used. It was spcacious, modern and spotless with a gorgeous bathroom that would rival any 5 star hotel. On our way out we saw inside some of the vacated rooms and it seems the size and shape of the rooms vary quite a bit. We will definitely stay here another time. Just a 5 minute walk and you're right in the heart of Newmarket. From Gillyflower5
+
+4th April 2015 - Nice comfortable accommodation with good service. Good parking at the Inn and also handy for buses into the city. Handy to the trendy Parnell Shops and restaurants. The Auckland water front is a must to visit. From Jenni1949
+
+April 2015 - Excellent stay - very clean and comfortable. Which was exactly what I was looking for just to relax and have a peaceful sleep. From Ivana Vaea
+
+March 2015 - Stayed one night in Newmarket to attend the Eagles concert. The motel is close enough to go to Nuffield Street for lunch and to and from train station. Large comfortable room with all that we needed. Love the bed with the windows closed it was very quiet. We stayed in Room 11 - 1st floor away from the road. Definitely stay if we needed a base in Auckland. From Kiwisue629
+
+March 2015 - The people that run the hotel are just delightful and so accommodating and helpful. From Margaret Gott
+
+February 2015 - Kitchen facilities were excellent, as was WIFI. Our room was spotlessly clean and very comfortable. Picked up the coast to coast footpath and walked into Auckland centre via Mount Eden and Auckland Domain, brilliant 2 hour walk. From Susan
+
+January 2015 - My family have stayed here a number of times. We have always found the service to be of the highest quality and the staff particularly friendly. From Maree Hood
+
+10th November 2014 - Recommended. I will be definitely returning. Enjoyed our stay, warm and friendly atmosphere. Unfortunately only 4 star - Should be 5 star. Room was excellent, it had everything we needed. Hygiencily 100%. Quiet location, rested well. Motel Family is ECO-Friendly - from William Ruru.
+
+June 2014 - To Lillian, Uini and all staff, Thank you very much for looking after me so well these last 4 months. All the very best. Ed Butler
+
+March 2014 - Room 9 is a wonderful room to stay in. Very quiet, with comfortable beds and lovely and clean. My children and I had a perfect nights sleep and everyone was very happy. Thank you JM
+
+Excellent stay for 3 nights at Alexander Inn - Room 17. Clean, comfortable and everything I needed. The most comfortable bed I have slept in away from home. Lillian on reception was extremely helpful and friendly - nothing was too much trouble. Highly recommended and would definitely stay again.
+
+Good morning - I have returned to Masterton after staying in Auckland at the Alexander Inn on Thursday night. I would to thank you for making our stay very enjoyable and for your generosity in letting us leave our car in your car park all day on Friday. We were shopping for a wedding and to not have to worry about moving the car was a considerable help. We did appreciate it and your friendly service. Ruth Eckford 19th March 2013
+
+8th September 2013 - Enjoyed our stay! Room spotless best we have ever stayed in! Staff very pleasant. Thank you KBS
+
+Room 1 - Rooms are lovely, clean and modern. People friendly and helpful. Great here free parking too - 1st Feb 2013 - The Qualmark star grading system influenced our decision to stay here.
+
+Room 3 - What a wonderful way to start our NZ holiday. The king sized bed was ideal for mature couples like us (who need good lumber support) with its luxurious coveralet & plenty of room to get lost in. The appointments were spotlessly clean with options to dine in if required. In short we were very impressed. Congratulations to the staff who ensure its standards are maintained. We certainly will be back. Regards, Jocelyn & Dan Teka Gold Coast - PS I'm so thrilled to say that the photographs on the internet are exactly what you get, not a 10 year old photo.
+
+Room 10 - Suitably impressed - excellent, topgrade, very clean! Quiet, would recommend this - decor excellent.
+
+Room 15 - Fantastic stay the beds are the best we have had in a motel. Very clean & super quiet. Fabulous location. Andrea & Garry Foot - 1/11/12
+
+Room 21 - As with previous occasions staying here. The hospitality, welcome, room cleanliness, preparation is excellent I have no hesitation to return and recommend to others of my experience of Alexander Inn. Location close to city, hospitals, motorways, airport, restaurants, & quiet situation adds to my reason to choose Alexander Inn. Robin Sisson - Tauranga 23/10/12
+
+Room 25 - Loved the place and the staff - Sydney Gustafen 6/10/12
+
+Room 3 - Perfect for a business stay - Double glazed windows ensured absolute quietness, spotless clean - C.Hickey 7/8/12
+
+Room 6 - Next time I need to stay at a motel in New market I will stay at Alexander Inn. It had everything I required and is close to the shops only walking distance away. I also liked the style of the room and felt warm and comfortable, the pillows and bed were amazing. I will be recommending Alexander Inn to colleagues, friends and family. Kellie Hodgson 1 & 2/7/12
+        </p>
       </main>
     );
   }
