97d4de190cb09922f2519e33ae910fa1aa642238
Barry Chen <barry.phasel235@gmail.com>
2026-03-07 21:31:54 -0500

    added news hero

--- src/app/news/page.tsx
+++ src/app/news/page.tsx
@@ -1,7 +1,18 @@
+import Image from "next/image";
+
 export default function News() {
     return (
-      <main className="pt-16">
-        News content
+      <main className="pt-4">
+        <Image
+          src="/images/0.jpg"
+          alt="newshero"
+          width={1600}
+          height={800}
+          className="w-full object-cover h-[640px]"
+        />
+        <h1 className="text-4xl">
+          NEWS
+        </h1>
       </main>
     );
   }
